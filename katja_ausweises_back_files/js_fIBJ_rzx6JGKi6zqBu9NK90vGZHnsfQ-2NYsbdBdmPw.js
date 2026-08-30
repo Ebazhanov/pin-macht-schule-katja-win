6e1cdc42c9c5d11460727dfd3dca2e7b04ad307b
Evgenii Bazhanov <evg.bazhanov@yahoo.com>
2026-08-30 12:30:35 +0200

    init

---
+++ katja_ausweises_back_files/js_fIBJ_rzx6JGKi6zqBu9NK90vGZHnsfQ-2NYsbdBdmPw.js
@@ -0,0 +1,197 @@
+Drupal.locale = { 'pluralFormula': function ($n) { return Number(($n!=1)); }, 'strings': {"":{"An AJAX HTTP error occurred.":"Ein AJAX-HTTP-Fehler ist aufgetreten.","HTTP Result Code: !status":"HTTP-R\u00fcckgabe-Code: !status","An AJAX HTTP request terminated abnormally.":"Eine AJAX-Anfrage ist abnormal beendet worden.","Debugging information follows.":"Im Folgenden finden Sie Debugging-Informationen.","Path: !uri":"Pfad: !uri","StatusText: !statusText":"Statustext: !statusText","ResponseText: !responseText":"Antworttext: !responseText","ReadyState: !readyState":"ReadyState: !readyState","Hide":"Ausblenden","Show":"Anzeigen","Show shortcuts":"Verkn\u00fcpfungen anzeigen","Hide shortcuts":"Verkn\u00fcpfungen ausblenden","Re-order rows by numerical weight instead of dragging.":"Zeilen mittels numerischer Gewichtung ordnen statt mit Drag-and-Drop.","Show row weights":"Zeilenreihenfolge anzeigen","Hide row weights":"Zeilenreihenfolge ausblenden","Drag to re-order":"Ziehen, um die Reihenfolge zu \u00e4ndern","Changes made in this table will not be saved until the form is submitted.":"\u00c4nderungen in dieser Tabelle werden nicht gespeichert, bis dieses Formular abgesendet wurde.","Media browser":"Medien-Browser","(active tab)":"(aktiver Reiter)","Enabled":"Aktiviert","Sun":"So.","This field is required.":"Diese Feld ist erforderlich.","@count months":"@count Monate","@count weeks":"@count Wochen","Edit":"Bearbeiten","Shortcuts":"Verkn\u00fcpfungen","@title dialog":"@title Dialog","Status":"Status","Loading":"Laden","Select all rows in this table":"Alle Zeilen dieser Tabelle ausw\u00e4hlen","Deselect all rows in this table":"Alle Zeilen dieser Tabelle abw\u00e4hlen","Disabled":"Deaktiviert","1 hour":"1 Stunde","@count min":"@count Minuten","Thu":"Do.","Thursday":"Donnerstag","@count years":"@count Jahre","Friday":"Freitag","Sunday":"Sonntag","Monday":"Montag","Tuesday":"Dienstag","Wednesday":"Mittwoch","Saturday":"Samstag","Configure":"Konfigurieren","Next":"Weiter","Cancel":"Abbrechen","Size":"Gr\u00f6\u00dfe","none":"keine","Add":"Hinzuf\u00fcgen","Filename":"Dateiname","All":"Alle","Done":"Fertig","@count hours":"@count Stunden","1 day":"1 Tag","@count days":"@count Tage","Selected":"Ausgew\u00e4hlt","N\/A":"n. v.","OK":"OK","Prev":"Vorheriges","Mon":"Mo.","Tue":"Di.","Wed":"Mi.","Fri":"Fr.","Sat":"Sa.","January":"Januar","February":"Februar","March":"M\u00e4rz","April":"April","May":"Mai","June":"Juni","July":"Juli","August":"August","September":"September","October":"Oktober","November":"November","December":"Dezember","Today":"Heute","Jan":"Jan.","Feb":"Feb","Mar":"M\u00e4rz","Apr":"Apr.","Jun":"Juni","Jul":"Juli","Aug":"Aug.","Sep":"Sep","Oct":"Okt.","Nov":"Nov.","Dec":"Dez.","Su":"So","Mo":"Mo","Tu":"Di","We":"Mi","Th":"Do","Fr":"Fr","Sa":"Sa","Not published":"Nicht ver\u00f6ffentlicht","Please wait...":"Bitte warten...","1 year":"1 Jahr","1 week":"1 Woche","1 min":"1 Minute","1 sec":"1 Sekunde","@count sec":"@count Sekunden","mm\/dd\/yy":"dd.mm.yy","1 month":"1 Monat","By @name on @date":"Von @name am @date","By @name":"Von @name","Not in menu":"Nicht im Men\u00fc","Alias: @alias":"Alias: @alias","No alias":"Kein Alias","0 sec":"0 Sek.","New revision":"Neue Revision","The changes to these blocks will not be saved until the \u003Cem\u003ESave blocks\u003C\/em\u003E button is clicked.":"Die \u00c4nderungen an diesen Bl\u00f6cken werden nicht gespeichert, bis auf dem \u003Cem\u003EBl\u00f6cke speichern\u003C\/em\u003E-Button geklickt wurde.","This permission is inherited from the authenticated user role.":"Diese Berechtigung wird von der Rolle \u201aAuthentifizierte Benutzer\u2018 ererbt.","No revision":"Keine Revision","@number comments per page":"@number Kommentare pro Seite","Requires a title":"Ben\u00f6tigt einen Titel","Not restricted":"Uneingeschr\u00e4nkt","Not customizable":"Nicht anpassbar","Restricted to certain pages":"Auf bestimmte Seiten eingeschr\u00e4nkt","The block cannot be placed in this region.":"Der Block kann nicht in dieser Region abgelegt werden.","Customize dashboard":"Dashboard anpassen","Hide summary":"Zusammenfassung ausblenden","Edit summary":"Zusammenfassung bearbeiten","Don\u0027t display post information":"Beitragsinformationen nicht anzeigen","The selected file %filename cannot be uploaded. Only files with the following extensions are allowed: %extensions.":"Die ausgew\u00e4hlte Datei %filename konnte nicht hochgeladen werden. Nur Dateien mit den folgenden Erweiterungen sind zul\u00e4ssig: %extensions.","Autocomplete popup":"Popup zur automatischen Vervollst\u00e4ndigung","Searching for matches...":"Suche \u2026","Loading...":"Wird geladen...","This will discard all unsaved changes. Are you sure?":"M\u00f6chten Sie alle ungespeicherten \u00c4nderungen verwerfen?","Select all":"Alle ausw\u00e4hlen","Submit":"Absenden","Cannot continue, nothing selected":"Zum Fortsetzen w\u00e4hlen Sie etwas aus","Also allow !name role to !permission?":"Der !name-Rolle !permission gew\u00e4hren?","Add file":"Datei hinzuf\u00fcgen","all":"Alle","Select":"Ausw\u00e4hlen","Loading token browser...":"Token-Browser wird geladen\u2026","Available tokens":"Verf\u00fcgbare Tokens","Insert this token into your form":"Dieses Token in Ihr Formular einf\u00fcgen","First click a text field to insert your tokens into.":"Klicken Sie zun\u00e4chst auf ein Textfeld, um Ihr Token einzuf\u00fcgen.","Automatic alias":"Automatisches Alias","List additional actions":"Weitere Aktionen auflisten","CustomMessage: !customMessage":"Benutzerdefinierte Nachricht: !customMessage","The response failed verification so will not be processed.":"Die Verifizierung der Antwort ist fehlgeschlagen, deshalb wird die Verarbeitung nicht fortgef\u00fchrt.","The callback URL is not local and not trusted: !url":"Die Callback-URL ist nicht lokal und nicht vertrauensw\u00fcrdig: !url","Remove group":"Gruppe entfernen","Apply (all displays)":"Anwenden (alle Anzeigen)","Revert to default":"Auf Standardwert zur\u00fccksetzen","Apply (this display)":"Anwenden (diese Anzeige)","No results":"Keine Ergebnisse","clear":"l\u00f6schen","New":"Neu","Modules installed within the last week.":"In der letzten Woche installierte Module.","No modules added within the last week.":"In der letzten Woche wurden keine neuen Module hinzugef\u00fcgt.","Recent":"Neueste","Modules enabled\/disabled within the last week.":"Aktivierte und deaktivierte Module innerhalb der letzten Woche.","No modules were enabled or disabled within the last week.":"In der letzten Woche wurden keine Module aktiviert oder deaktiviert.","@enabled of @total":"@enabled von @total","Ignored from settings":"Aufgrund von Einstellungen ignoriert","Inclusion: @value":"Einbeziehen: @value","Priority: @value":"Priorit\u00e4t: @value","One domain with multiple subdomains":"Eine Domain mit mehreren Sub-Domains","Multiple top-level domains":"Mehrere Top-Level-Domains","Universal web tracking opt-out":"Universeller Opt-out aus dem Web-Tracking","All pages with exceptions":"Alle Seiten mit Ausnahmen","Excepted: @roles":"Ausgenommen: @roles","On by default with opt out":"Standardm\u00e4\u00dfig aktiv, mit Opt-out","Off by default with opt in":"Standardm\u00e4\u00dfig deaktiviert, mit Opt-in","Outbound links":"Ausgehende Links","Mailto links":"Mailto-Links","Downloads":"Downloads","Not tracked":"Nicht verfolgt","@items enabled":"@items aktiviert","Site search":"Website-Suche","AdSense ads":"AdSense-Anzeigen","A single domain":"Eine einzelne Domain","Anonymize IP":"IP anonymisieren","No privacy":"Keine Privatsph\u00e4re","@label: @value":"@label: @value","Using defaults":"Standardeinstellungen verwenden","No redirects":"Keine Weiterleitungen","1 enabled redirect":"1 aktivierte Umleitung","@count enabled redirects":"@count aktivierte Umleitungen","1 disabled redirect":"1 deaktivierte Umleitung","@count disabled redirects":"@count deaktivierte Umleitungen","Scheduled for publishing":"Geplante ver\u00f6ffentlichung","Scheduled for unpublishing":"Geplante Archivierung","Not scheduled":"Nicht geplant","Publishing enabled":"Ver\u00f6ffentlichen aktiviert","Unpublishing enabled":"Archivierung aktiviert","Show description":"Beschreibung anzeigen","Hide description":"Beschreibung ausblenden","Start upload":"Hochladen beginnen","Close":"Schlie\u00dfen","Colorbox":"Colorbox","Link attribution":"Linkzuordnung","URL fragments":"URL-Fragmente","Deselect all":"Alles abw\u00e4hlen","Select files":"Dateien ausw\u00e4hlen","Add files to the upload queue and click the start button.":"F\u00fcgen Sie Dateien zum Hochladen hinzu und Klicken Sie den Startbutton.","Add files":"Dateien hinzuf\u00fcgen","Stop current upload":"Laufenden Upload abbrechen","Start uploading queue":"Mit Hochladen beginnen","Uploaded %d\/%d files":"%d\/%d Dateien hochgeladen","Drag files here.":"Dateien hier hinein ziehen (Drag \u0026 Drop).","File extension error.":"Dateierweiterung-Fehler","File size error.":"Dateigr\u00f6\u00dfe-Fehler","Init error.":"Initialisierungsfehler","HTTP Error.":"HTTP-Fehler.","Security error.":"Sicherheitsfehler","Generic error.":"Allgemeiner Fehler.","IO error.":"IO-Fehler.","Stop upload":"Upload abbrechen.","%d files queued":"%d Dateien in der Warteschlange","File: %s":"Datei: %s","Using runtime: ":"Verwende Laufzeit: ","File: %f, size: %s, max file size: %m":"Datei: %f, Gr\u00f6\u00dfe: %s, Maximale Dateigr\u00f6\u00dfe: %m","Upload element accepts only %d file(s) at a time. Extra files were stripped.":"Das Upload-Element erlaubt nur %d Datei(en) gleichzeitig. Weitere Dateien wurden entfernt.","Error: File too large: ":"Fehler: Datei zu gro\u00df: ","Error: Invalid file extension: ":"Fehler: Unerlaubte Dateierweiterung: ","File count error.":"Fehler in der Dateianzahl.","Please wait while your files are being uploaded.":"Dateien werden hochgeladen. Bitte warten."}} };;
+(function ($) {
+
+Drupal.googleanalytics = {};
+
+$(document).ready(function() {
+
+  // Attach mousedown, keyup, touchstart events to document only and catch
+  // clicks on all elements.
+  $(document.body).bind("mousedown keyup touchstart", function(event) {
+
+    // Catch the closest surrounding link of a clicked element.
+    $(event.target).closest("a,area").each(function() {
+
+      // Is the clicked URL internal?
+      if (Drupal.googleanalytics.isInternal(this.href)) {
+        // Skip 'click' tracking, if custom tracking events are bound.
+        if ($(this).is('.colorbox') && (Drupal.settings.googleanalytics.trackColorbox)) {
+          // Do nothing here. The custom event will handle all tracking.
+          //console.info("Click on .colorbox item has been detected.");
+        }
+        // Is download tracking activated and the file extension configured for download tracking?
+        else if (Drupal.settings.googleanalytics.trackDownload && Drupal.googleanalytics.isDownload(this.href)) {
+          // Download link clicked.
+          ga("send", {
+            "hitType": "event",
+            "eventCategory": "Downloads",
+            "eventAction": Drupal.googleanalytics.getDownloadExtension(this.href).toUpperCase(),
+            "eventLabel": Drupal.googleanalytics.getPageUrl(this.href),
+            "transport": "beacon"
+          });
+        }
+        else if (Drupal.googleanalytics.isInternalSpecial(this.href)) {
+          // Keep the internal URL for Google Analytics website overlay intact.
+          ga("send", {
+            "hitType": "pageview",
+            "page": Drupal.googleanalytics.getPageUrl(this.href),
+            "transport": "beacon"
+          });
+        }
+      }
+      else {
+        if (Drupal.settings.googleanalytics.trackMailto && $(this).is("a[href^='mailto:'],area[href^='mailto:']")) {
+          // Mailto link clicked.
+          ga("send", {
+            "hitType": "event",
+            "eventCategory": "Mails",
+            "eventAction": "Click",
+            "eventLabel": this.href.substring(7),
+            "transport": "beacon"
+          });
+        }
+        else if (Drupal.settings.googleanalytics.trackOutbound && this.href.match(/^\w+:\/\//i)) {
+          if (Drupal.settings.googleanalytics.trackDomainMode !== 2 || (Drupal.settings.googleanalytics.trackDomainMode === 2 && !Drupal.googleanalytics.isCrossDomain(this.hostname, Drupal.settings.googleanalytics.trackCrossDomains))) {
+            // External link clicked / No top-level cross domain clicked.
+            ga("send", {
+              "hitType": "event",
+              "eventCategory": "Outbound links",
+              "eventAction": "Click",
+              "eventLabel": this.href,
+              "transport": "beacon"
+            });
+          }
+        }
+      }
+    });
+  });
+
+  // Track hash changes as unique pageviews, if this option has been enabled.
+  if (Drupal.settings.googleanalytics.trackUrlFragments) {
+    window.onhashchange = function() {
+      ga("send", {
+        "hitType": "pageview",
+        "page": location.pathname + location.search + location.hash
+      });
+    };
+  }
+
+  // Colorbox: This event triggers when the transition has completed and the
+  // newly loaded content has been revealed.
+  if (Drupal.settings.googleanalytics.trackColorbox) {
+    $(document).bind("cbox_complete", function () {
+      var href = $.colorbox.element().attr("href");
+      if (href) {
+        ga("send", {
+          "hitType": "pageview",
+          "page": Drupal.googleanalytics.getPageUrl(href)
+        });
+      }
+    });
+  }
+
+});
+
+/**
+ * Check whether the hostname is part of the cross domains or not.
+ *
+ * @param string hostname
+ *   The hostname of the clicked URL.
+ * @param array crossDomains
+ *   All cross domain hostnames as JS array.
+ *
+ * @return boolean
+ */
+Drupal.googleanalytics.isCrossDomain = function (hostname, crossDomains) {
+  /**
+   * jQuery < 1.6.3 bug: $.inArray crushes IE6 and Chrome if second argument is
+   * `null` or `undefined`, https://bugs.jquery.com/ticket/10076,
+   * https://github.com/jquery/jquery/commit/a839af034db2bd934e4d4fa6758a3fed8de74174
+   *
+   * @todo: Remove/Refactor in D8
+   */
+  if (!crossDomains) {
+    return false;
+  }
+  else {
+    return $.inArray(hostname, crossDomains) > -1 ? true : false;
+  }
+};
+
+/**
+ * Check whether this is a download URL or not.
+ *
+ * @param string url
+ *   The web url to check.
+ *
+ * @return boolean
+ */
+Drupal.googleanalytics.isDownload = function (url) {
+  var isDownload = new RegExp("\\.(" + Drupal.settings.googleanalytics.trackDownloadExtensions + ")([\?#].*)?$", "i");
+  return isDownload.test(url);
+};
+
+/**
+ * Check whether this is an absolute internal URL or not.
+ *
+ * @param string url
+ *   The web url to check.
+ *
+ * @return boolean
+ */
+Drupal.googleanalytics.isInternal = function (url) {
+  var isInternal = new RegExp("^(https?):\/\/" + window.location.host, "i");
+  return isInternal.test(url);
+};
+
+/**
+ * Check whether this is a special URL or not.
+ *
+ * URL types:
+ *  - gotwo.module /go/* links.
+ *
+ * @param string url
+ *   The web url to check.
+ *
+ * @return boolean
+ */
+Drupal.googleanalytics.isInternalSpecial = function (url) {
+  var isInternalSpecial = new RegExp("(\/go\/.*)$", "i");
+  return isInternalSpecial.test(url);
+};
+
+/**
+ * Extract the relative internal URL from an absolute internal URL.
+ *
+ * Examples:
+ * - https://mydomain.com/node/1 -> /node/1
+ * - https://example.com/foo/bar -> https://example.com/foo/bar
+ *
+ * @param string url
+ *   The web url to check.
+ *
+ * @return string
+ *   Internal website URL
+ */
+Drupal.googleanalytics.getPageUrl = function (url) {
+  var extractInternalUrl = new RegExp("^(https?):\/\/" + window.location.host, "i");
+  return url.replace(extractInternalUrl, '');
+};
+
+/**
+ * Extract the download file extension from the URL.
+ *
+ * @param string url
+ *   The web url to check.
+ *
+ * @return string
+ *   The file extension of the passed url. e.g. "zip", "txt"
+ */
+Drupal.googleanalytics.getDownloadExtension = function (url) {
+  var extractDownloadextension = new RegExp("\\.(" + Drupal.settings.googleanalytics.trackDownloadExtensions + ")([\?#].*)?$", "i");
+  var extension = extractDownloadextension.exec(url);
+  return (extension === null) ? '' : extension[1];
+};
+
+})(jQuery);
+;
